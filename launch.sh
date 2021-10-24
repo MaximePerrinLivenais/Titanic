4efc720266224431a079d5d11a1fbef511e1c9b7
Nicolas Guerguadj <nicolas.guerguadj@epita.fr>
2021-10-24 16:43:25 +0200

    feat(launch): create server_logs folder in script
    Signed-off-by: Nicolas Guerguadj <nicolas.guerguadj@epita.fr>

--- launch.sh
+++ launch.sh
@@ -3,6 +3,10 @@ read -p 'Choose the number of clients: ' nb_clients
 
 nb_process=$((1 + nb_servers + nb_clients))
 
+if [ ! -d "server_logs" ]; then
+    mkdir server_logs
+fi
+
 echo "localhost slots=$nb_process" > hostfile
 
 mpirun -hostfile hostfile --mca opal_warn_on_missing_libcuda 0 ./titanic $nb_servers $nb_clients
